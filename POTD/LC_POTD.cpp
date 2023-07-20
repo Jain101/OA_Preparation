0bcfde485471018b7f0473dccb131c5ddc09db21
Jain101 <2021it1ja@mitsgwl.ac.in>
2023-07-20 14:26:59 +0530

    Update LC_POTD.cpp

--- POTD/LC_POTD.cpp
+++ POTD/LC_POTD.cpp
@@ -7,7 +7,7 @@ public:
     /*
      * Date : 19.07.2023
      * Problem : https://leetcode.com/problems/non-overlapping-intervals/description/
-     * Approach : Greedy
+     * Approach : Knapsack problem just sort by end and count as per the condition
      */
     static bool cmp(vector<int> &a, vector<int> &b)
     {
@@ -37,5 +37,48 @@ public:
      * Analysis
      * TC - O(nlogn) + O(n) ~ O(nlogn)
      * SC - O(logn)[STL sort() uses a hybrid of Quick, Heap and Insertion Sort, with a WC SC O(log⁡n)]
+     */
+
+    /*
+     * Date : 20.07.2023
+     * Problem : https://leetcode.com/problems/asteroid-collision/post-solution/?submissionId=999179017
+     * Approach : Stack problem so just consider all the cases and code it up
+     */
+    vector<int> asteroidCollision(vector<int> &asteroids)
+    {
+        stack<int> st;
+        vector<int> ans;
+        for (int &ast : asteroids)
+        {
+            int flag = 1;
+            while (!st.empty() && (st.top() > 0 && ast < 0))
+            {
+                int check = st.top() - abs(ast);
+                if (check < 0)
+                {
+                    st.pop();
+                    ans.pop_back();
+                    continue;
+                }
+                else if (check == 0)
+                {
+                    st.pop();
+                    ans.pop_back();
+                }
+                flag = 0;
+                break;
+            }
+            if (flag)
+            {
+                st.push(ast);
+                ans.push_back(ast);
+            }
+        }
+        return ans;
+    }
+    /*
+     * Analysis
+     * TC - O(2*n) ~ O(n) : for every asteroid, we might do 2 operations at max - push and pop into the stack and that too only once.
+     * SC - O(n) : worst case when no collision all are pushed into stack
      */
 };
